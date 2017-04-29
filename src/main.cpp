701b34558ed6ea13e288010fdaf441dde008e2e6
Allex Lima <allexlima97@gmail.com>
2017-04-29 15:41:09 -0400

    finishing main code

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,19 @@
 Sound sound(13, 10, 9);
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
-void mario(){
-    //sound.mario();
-    lcd.print("Playing Mario Bros");
-    lcd.setCursor(0, 1);
-}
-
-void st(){
-    //sound.starwars();
-    lcd.print("Playing Star Wars");
-    lcd.setCursor(0, 1);
-}
-
 void setup(){
      lcd.begin(16, 2);
-     lcd.print("Playing Star Wars");
      lcd.setCursor(0, 1);
 }
 
 void loop(){
-
+    if(sound.onPushMario()){
+        sound.mario();
+        lcd.print("Playing Mario Bros");
+        lcd.setCursor(0, 1);
+    }else if(sound.onPushSt()){
+        sound.starwars();
+        lcd.print("Playing Star Wars");
+        lcd.setCursor(0, 1);
+    }
 }
